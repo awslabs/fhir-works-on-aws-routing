8bf5fd820982baf35963196c95d57a07398b4903
Tim Nguyen <thingut@amazon.com>
2021-02-11 11:46:22 -0500

    Add apache2 license at top of new files

--- src/router/validation/validationUtilities.test.ts
+++ src/router/validation/validationUtilities.test.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import { InvalidResourceError, Validator } from 'fhir-works-on-aws-interface';
 import { validateResource } from './validationUtilities';
 
--- src/router/validation/validationUtilities.ts
+++ src/router/validation/validationUtilities.ts
@@ -1,3 +1,8 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
 import { Validator } from 'fhir-works-on-aws-interface';
 
 export async function validateResource(validators: Validator[], resource: any): Promise<void> {
