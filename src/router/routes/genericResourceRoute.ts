5a87d2bc11615392d1535ce029db61bc3cd9d17f
chrisbednarski <chris.bednarski@outlook.com>
2021-03-19 12:08:13 +1100

    fix: Relax validation rules before a patch can be applied (#64)
    Co-authored-by: chris_bednarski <Chris.Bednarski@minfos.com.au>

--- src/router/routes/genericResourceRoute.ts
+++ src/router/routes/genericResourceRoute.ts
@@ -218,7 +218,7 @@ export default class GenericResourceRoute {
                     const { id } = req.params;
                     const { body } = req;
 
-                    if (body.id === null || body.id !== id) {
+                    if (body.id && body.id !== id) {
                         throw new createError.BadRequest(
                             `Can not update resource with ID[${id}], while the given request payload has an ID[${body.id}]`,
                         );
