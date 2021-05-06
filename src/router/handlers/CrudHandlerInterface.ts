4b451cc0af7030a91fde9f0325acab28a7979071
Nestor Carvantes <carvantes@gmail.com>
2021-05-05 23:23:42 -0700

    refactor: consolidate search logic inside ResourceHandler (#77)

--- src/router/handlers/CrudHandlerInterface.ts
+++ src/router/handlers/CrudHandlerInterface.ts
@@ -12,7 +12,7 @@ export default interface CrudHandlerInterface {
     read(resourceType: string, id: string): any;
     vRead(resourceType: string, id: string, vid: string): any;
     delete(resourceType: string, id: string): any;
-    typeSearch(resourceType: string, searchParams: any, allowedResourceTypes: string[], userIdentity: KeyValueMap): any;
+    typeSearch(resourceType: string, searchParams: any, userIdentity: KeyValueMap): any;
     typeHistory(resourceType: string, searchParams: any, userIdentity: KeyValueMap): any;
     instanceHistory(resourceType: string, id: string, searchParams: any, userIdentity: KeyValueMap): any;
 }
