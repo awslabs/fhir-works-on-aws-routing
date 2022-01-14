4d764335d883e2248b2458909100dfaf51f6dcc6
Mike Lohmeier <mike.lohmeier@resmed.com>
2022-01-14 09:10:36 -0600

    feat: Add tests for building server url used with alternateUrls

--- src/router/middlewares/setServerUrl.test.ts
+++ src/router/middlewares/setServerUrl.test.ts
@@ -59,4 +59,79 @@ describe('createServerUrlMiddleware', () => {
         expect(nextMock).toHaveBeenCalledWith();
         expect(res.locals.serverUrl).toEqual('https://fwoa.com/some/path');
     });
+
+    test('alternativeUrl host used', async () => {
+        const fhirConfig = {
+            server: {
+                url: 'https://fwoa.com',
+                alternativeUrls: ['https://private.api.gateway.example.com', 'https://custom.dns.example.com'],
+            },
+        } as FhirConfig;
+
+        const serverUrlMiddleware = setServerUrlMiddleware(fhirConfig);
+
+        const nextMock = jest.fn();
+        const req = {
+            baseUrl: '/',
+            headers: { host: 'private.api.gateway.example.com' },
+        } as unknown as express.Request;
+        const res = {
+            locals: {},
+        } as unknown as express.Response;
+
+        await serverUrlMiddleware(req, res, nextMock);
+
+        expect(nextMock).toHaveBeenCalledTimes(1);
+        expect(nextMock).toHaveBeenCalledWith();
+        expect(res.locals.serverUrl).toEqual('https://private.api.gateway.example.com');
+    });
+
+    test('alternativeUrl host used and path appended', async () => {
+        const fhirConfig = {
+            server: {
+                url: 'https://fwoa.com',
+                alternativeUrls: ['https://private.api.gateway.example.com', 'https://custom.dns.example.com'],
+            },
+        } as FhirConfig;
+
+        const serverUrlMiddleware = setServerUrlMiddleware(fhirConfig);
+
+        const nextMock = jest.fn();
+        const req = {
+            baseUrl: '/some/path',
+            headers: { host: 'private.api.gateway.example.com' },
+        } as unknown as express.Request;
+        const res = {
+            locals: {},
+        } as unknown as express.Response;
+
+        await serverUrlMiddleware(req, res, nextMock);
+
+        expect(nextMock).toHaveBeenCalledTimes(1);
+        expect(nextMock).toHaveBeenCalledWith();
+        expect(res.locals.serverUrl).toEqual('https://private.api.gateway.example.com/some/path');
+    });
+
+    test('alternativeUrl host not used with no match', async () => {
+        const fhirConfig = {
+            server: {
+                url: 'https://fwoa.com',
+                alternativeUrls: ['https://private.api.gateway.example.com', 'https://custom.dns.example.com'],
+            },
+        } as FhirConfig;
+
+        const serverUrlMiddleware = setServerUrlMiddleware(fhirConfig);
+
+        const nextMock = jest.fn();
+        const req = { baseUrl: '/', headers: { host: 'something.else.example.com' } } as unknown as express.Request;
+        const res = {
+            locals: {},
+        } as unknown as express.Response;
+
+        await serverUrlMiddleware(req, res, nextMock);
+
+        expect(nextMock).toHaveBeenCalledTimes(1);
+        expect(nextMock).toHaveBeenCalledWith();
+        expect(res.locals.serverUrl).toEqual('https://fwoa.com');
+    });
 });
--- src/router/middlewares/setServerUrl.ts
+++ src/router/middlewares/setServerUrl.ts
@@ -7,8 +7,8 @@
 
 import { FhirConfig } from 'fhir-works-on-aws-interface';
 import express from 'express';
-import RouteHelper from '../routes/routeHelper';
 import { isEmpty, isUndefined } from 'lodash';
+import RouteHelper from '../routes/routeHelper';
 
 /**
  * Sets the value of `res.locals.serverUrl`
@@ -18,16 +18,17 @@ export const setServerUrlMiddleware: (
     fhirConfig: FhirConfig,
 ) => (req: express.Request, res: express.Response, next: express.NextFunction) => void = (fhirConfig: FhirConfig) => {
     return RouteHelper.wrapAsync(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-        let serverUrl = fhirConfig.server.url
-        if (!isEmpty(fhirConfig.server.alternativeUrls)){
-            const hostMatch = fhirConfig.server.alternativeUrls?.find((alternativeUrl: string)=>{
-                return alternativeUrl === req.headers.host;
+        let serverUrl = fhirConfig.server.url;
+        if (!isEmpty(fhirConfig.server.alternativeUrls)) {
+            const hostMatch = fhirConfig.server.alternativeUrls?.find((alternativeUrl: string) => {
+                const myUrl = new URL(alternativeUrl);
+                return myUrl.hostname === req.headers.host;
             });
-            if (!isUndefined(hostMatch)){
-                serverUrl = hostMatch; 
+            if (!isUndefined(hostMatch)) {
+                serverUrl = hostMatch;
             }
         }
-        
+
         if (req.baseUrl && req.baseUrl !== '/') {
             res.locals.serverUrl = serverUrl + req.baseUrl;
         } else {
